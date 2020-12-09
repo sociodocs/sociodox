98c2a99156590aa9ac3118c01b2cc3da73864d93
utkarshbelkhede <utkarshbelkhede@gmail.com>
2020-12-10 00:19:59 +0530

    setup multiple connection for db

--- php/database.php
+++ php/database.php
@@ -1,11 +1,8 @@
 <?php
 
-
 $conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1998")
-or die("Could not connect to server\n");
+or $conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1234")
+or $conn=pg_connect("host=localhost dbname=venom user=utkarsh port=5432 password=264064")
+or die("Could not connect");
 
-/*
-$conn=pg_connect("host=localhost dbname=venom user=utkarsh port=5432 password=264064")
-or die("Could not connect to server\n");
-*/
 ?>
