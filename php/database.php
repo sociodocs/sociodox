48d0bbcdbd6f060ffd7fb90d4e19de44de59a2e4
Aditya Karale <akarale61@gmail.com>
2021-01-05 19:24:58 +0530

    module

--- php/database.php
+++ php/database.php
@@ -1,7 +1,7 @@
 <?php
 
     $conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1998")
-    or $conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1234")
+    or $conn=pg_connect("host=localhost dbname=postgres user=postgres port=5432 password=1998")
     or $conn=pg_connect("host=localhost dbname=venom user=utkarsh port=5432 password=264064")
     or die("Could not connect");
 
